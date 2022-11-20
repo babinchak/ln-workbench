00295c705a002e45571bdd96b1bfeea245b9609a
Conner Babinchak <connerbabs@gmail.com>
2022-11-19 23:18:19 -0500

    Add tailwind

---
+++ frontend/pages/tx.tsx
@@ -0,0 +1,15 @@
+
+export default function Tx() {
+    return (
+        <>
+            <div className="border-white border-2">
+                <div>Script:</div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <div>Hex:</div>
+                <textarea></textarea>
+            </div>
+        </>
+    )
+}
